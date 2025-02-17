79699410b1d3d7dba87bb49f463331ac9b3ab9d5
Chebikik <24b_rim@liceum.ztu.edu.ua>
2025-02-17 11:20:28 +0200

    gsr

---
+++ script.js
@@ -0,0 +1,5 @@
+
+console.log('Script connected');
+document.getElementById('btn_wishes').addEventListener('click', () => {
+alert('Button clicked');
+document.getElementById ('p_wishes').innerText = 'Kaneu';})
